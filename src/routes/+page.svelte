e97ba272a7f28ec325baeb8839f3c482213005cf
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-06-28 02:22:00 +0700

    fetch basic stats

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    export let data;
+
+    const { fullName: name, primaryNumber: number } = data.player;
+    const { name: team } = data.team;
+
+    let stats = {};
+
+    data.stats.forEach(stat => {
+        stats[stat.group.displayName] = stat.splits[0].stat;
+    });
+</script>
+
+<h1>{name} #{number}</h1>
+<h2>{team}</h2>
+
+<br>
+
+<h3>Hitting</h3>
+<table border>
+    <thead>
+        <tr>
+            <th>HR</th>
+            <th>RBI</th>
+            <th>SB</th>
+            <th>AVG</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>{stats["hitting"].homeRuns}</td>
+            <td>{stats["hitting"].rbi}</td>
+            <td>{stats["hitting"].stolenBases}</td>
+            <td>{stats["hitting"].avg}</td>
+        </tr>
+    </tbody>
+</table>
+
+<h3>Pitching</h3>
+<table border>
+    <thead>
+        <tr>
+            <th>W</th>
+            <th>ERA</th>
+            <th>SO</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>{stats["pitching"].wins}</td>
+            <td>{stats["pitching"].era}</td>
+            <td>{stats["pitching"].strikeOuts}</td>
+        </tr>
+    </tbody>
+</table>
