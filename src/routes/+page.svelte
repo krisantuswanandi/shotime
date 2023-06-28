e467739e9149f556a7aa276603ed16a7d352a8a6
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-06-29 01:21:39 +0700

    styling update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,45 +2,52 @@
 	export let data;
 </script>
 
-<h1>{data.name} #{data.number}</h1>
-<h2>{data.team}</h2>
-
-<br />
-
-<h3>Hitting</h3>
-<table border={1}>
-	<thead>
-		<tr>
-			<th>HR</th>
-			<th>RBI</th>
-			<th>SB</th>
-			<th>AVG</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td>{data.stats.hitting.homeRuns}</td>
-			<td>{data.stats.hitting.rbi}</td>
-			<td>{data.stats.hitting.stolenBases}</td>
-			<td>{data.stats.hitting.avg}</td>
-		</tr>
-	</tbody>
-</table>
-
-<h3>Pitching</h3>
-<table border={1}>
-	<thead>
-		<tr>
-			<th>W</th>
-			<th>ERA</th>
-			<th>SO</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td>{data.stats.pitching.wins}</td>
-			<td>{data.stats.pitching.era}</td>
-			<td>{data.stats.pitching.strikeOuts}</td>
-		</tr>
-	</tbody>
-</table>
+<div class="flex justify-center items-center flex-col h-full">
+	<h1 class="text-4xl">{data.name}</h1>
+	<h2>{data.team} #{data.number}</h2>
+	<div class="mt-8">
+		<table class="bg-gray-200">
+			<caption>Hitting</caption>
+			<thead>
+				<tr>
+					<th class="px-4 py-1">HR</th>
+					<th class="px-4 py-1">RBI</th>
+					<th class="px-4 py-1">SB</th>
+					<th class="px-4 py-1">AVG</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td class="px-4 py-1">{data.stats.hitting.homeRuns}</td>
+					<td class="px-4 py-1">{data.stats.hitting.rbi}</td>
+					<td class="px-4 py-1">{data.stats.hitting.stolenBases}</td>
+					<td class="px-4 py-1">{data.stats.hitting.avg}</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="mt-4">
+		<table class="bg-gray-200">
+			<caption>Pitching</caption>
+			<thead>
+				<tr>
+					<th class="px-4 py-1">W</th>
+					<th class="px-4 py-1">ERA</th>
+					<th class="px-4 py-1">SO</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td class="px-4 py-1">{data.stats.pitching.wins}</td>
+					<td class="px-4 py-1">{data.stats.pitching.era}</td>
+					<td class="px-4 py-1">{data.stats.pitching.strikeOuts}</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="mt-10">
+		<a href="https://github.com/krisantuswanandi/shotime" target="_blank">
+			<img src="https://cdn.svgporn.com/logos/github-icon.svg" alt="github" class="w-6" />
+		</a>
+	</div>
+</div>
