1b78c00de3967e799618af71d6dc7988f8ee24e1
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-06-29 00:45:42 +0700

    forgot to use format smh

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,36 +1,38 @@
-const MLB_API = "https://statsapi.mlb.com/api/v1"
-const PLAYER_ID = "660271"
-const TEAM_ID = "108"
+const MLB_API = 'https://statsapi.mlb.com/api/v1';
+const PLAYER_ID = '660271';
+const TEAM_ID = '108';
 
 async function getPlayer() {
-	const response = await fetch(`${MLB_API}/people/${PLAYER_ID}`)
-	const result = await response.json()
+	const response = await fetch(`${MLB_API}/people/${PLAYER_ID}`);
+	const result = await response.json();
 
-	return result.people[0]
+	return result.people[0];
 }
 
 async function getTeam() {
-	const response = await fetch(`${MLB_API}/teams/${TEAM_ID}`)
-	const result = await response.json()
+	const response = await fetch(`${MLB_API}/teams/${TEAM_ID}`);
+	const result = await response.json();
 
-	return result.teams[0]
+	return result.teams[0];
 }
 
 async function getStats() {
-	const response = await fetch(`${MLB_API}/people/?personIds=${PLAYER_ID}&hydrate=stats(type=season,group=[hitting,pitching])`)
-	const result = await response.json()
+	const response = await fetch(
+		`${MLB_API}/people/?personIds=${PLAYER_ID}&hydrate=stats(type=season,group=[hitting,pitching])`
+	);
+	const result = await response.json();
 
-	return result.people[0].stats
+	return result.people[0].stats;
 }
 
 export async function load() {
-	const player = await getPlayer()
-	const team = await getTeam()
-	const stats = await getStats()
+	const player = await getPlayer();
+	const team = await getTeam();
+	const stats = await getStats();
 
 	return {
 		player,
 		team,
-		stats,
+		stats
 	};
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,55 @@
 <script>
-    export let data;
+	export let data;
 
-    const { fullName: name, primaryNumber: number } = data.player;
-    const { name: team } = data.team;
+	const { fullName: name, primaryNumber: number } = data.player;
+	const { name: team } = data.team;
 
-    let stats = {};
+	let stats = {};
 
-    data.stats.forEach(stat => {
-        stats[stat.group.displayName] = stat.splits[0].stat;
-    });
+	data.stats.forEach((stat) => {
+		stats[stat.group.displayName] = stat.splits[0].stat;
+	});
 </script>
 
 <h1>{name} #{number}</h1>
 <h2>{team}</h2>
 
-<br>
+<br />
 
 <h3>Hitting</h3>
 <table border>
-    <thead>
-        <tr>
-            <th>HR</th>
-            <th>RBI</th>
-            <th>SB</th>
-            <th>AVG</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>{stats["hitting"].homeRuns}</td>
-            <td>{stats["hitting"].rbi}</td>
-            <td>{stats["hitting"].stolenBases}</td>
-            <td>{stats["hitting"].avg}</td>
-        </tr>
-    </tbody>
+	<thead>
+		<tr>
+			<th>HR</th>
+			<th>RBI</th>
+			<th>SB</th>
+			<th>AVG</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>{stats['hitting'].homeRuns}</td>
+			<td>{stats['hitting'].rbi}</td>
+			<td>{stats['hitting'].stolenBases}</td>
+			<td>{stats['hitting'].avg}</td>
+		</tr>
+	</tbody>
 </table>
 
 <h3>Pitching</h3>
 <table border>
-    <thead>
-        <tr>
-            <th>W</th>
-            <th>ERA</th>
-            <th>SO</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>{stats["pitching"].wins}</td>
-            <td>{stats["pitching"].era}</td>
-            <td>{stats["pitching"].strikeOuts}</td>
-        </tr>
-    </tbody>
+	<thead>
+		<tr>
+			<th>W</th>
+			<th>ERA</th>
+			<th>SO</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>{stats['pitching'].wins}</td>
+			<td>{stats['pitching'].era}</td>
+			<td>{stats['pitching'].strikeOuts}</td>
+		</tr>
+	</tbody>
 </table>
